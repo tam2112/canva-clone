c59deb86978f7079a448349995f6951b8ed54e1e
Truong Minh Tam <tamtm1@imt-soft.com>
2025-07-24 19:22:15 +0700

    update utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable prefer-const */
 import { type ClassValue, clsx } from 'clsx';
 import qs from 'qs';
 import { twMerge } from 'tailwind-merge';
